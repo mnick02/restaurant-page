9087fc561e11988963761bc5e05d257170ca6185
mnick02 <nokiamn3@gmail.com>
2025-03-26 13:13:46 -0700

    Create function homepage and export so everything can be run with an eventListener

--- src/content.js
+++ src/content.js
@@ -1,25 +1,28 @@
 import SanjiImage from './sanji.png';
-export const content = document.getElementById("content");
 
-export const head = document.createElement("h1");
-head.classList.add("header");
-head.textContent = "Hello, this is the best restaurant";
-content.appendChild(head);
+export function homePage() {
+    const content = document.getElementById("content");
 
-export const paraText = document.createElement("p");
-paraText.classList.add("text");
-paraText.textContent = "Come eat some of the most fantastic food in the whole world at...";
-content.appendChild(paraText);
+    const head = document.createElement("h1");
+    head.classList.add("header");
+    head.textContent = "Hello, this is the best restaurant";
+    content.appendChild(head);
 
-export const newPara = document.createElement("p");
-newPara.style.fontStyle = 'italic';
-newPara.classList.add("text");
-newPara.textContent = "FOOD & STUFF!!!"
-content.appendChild(newPara);
+    const paraText = document.createElement("p");
+    paraText.classList.add("text");
+    paraText.textContent = "Come eat some of the most fantastic food in the whole world at...";
+    content.appendChild(paraText);
 
-export const image = document.createElement("img");
-image.classList.add("image");
-image.src = SanjiImage;
-image.width = 300;
-image.height = 200;
-content.appendChild(image);
+    const newPara = document.createElement("p");
+    newPara.style.fontStyle = 'italic';
+    newPara.classList.add("text");
+    newPara.textContent = "FOOD & STUFF!!!"
+    content.appendChild(newPara);
+
+    const image = document.createElement("img");
+    image.classList.add("image");
+    image.src = SanjiImage;
+    image.width = 300;
+    image.height = 200;
+    content.appendChild(image);
+}
