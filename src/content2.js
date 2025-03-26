ef61b26525db36fb71e8bef5d099c55cc6cb6d69
mnick02 <nokiamn3@gmail.com>
2025-03-26 12:36:47 -0700

    Create file for another tab

---
+++ src/content2.js
@@ -0,0 +1,7 @@
+export const two = document.createElement("div");
+two.classList.add("twoText");
+two.textContent = "Page Two";
+console.log("TOWEY");
+
+//Where to append to?
+//Where to export to?
