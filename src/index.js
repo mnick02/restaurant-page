94b8cd76d4b990c1cebd2016a5c3b5e39993e69c
mnick02 <nokiamn3@gmail.com>
2025-03-26 12:37:27 -0700

    Add eventListener for btn click to import new module to change tab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-import { head, paraText, newPara, image } from "./content";
+import { content, head, paraText, newPara, image } from "./content";
 
 console.log("Hello");
+
+const homeBtn = document.querySelector(".homeBtn");
+homeBtn.addEventListener("click", async () => {
+    try {
+        content.innerHTML = "";
+        const module = await import("./content2"); 
+        content.appendChild(module.two);
+        console.log("Hello");
+    }
+    catch (error) {
+        console.log("Error importing module: ", error);
+    }
+
+});
+
+//check if divcontent already exists from imprt
+//check if you can clear divcontent
+//dynamic imports?
