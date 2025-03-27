cb04d3919f07a644571544c1199be841218a52c4
mnick02 <nokiamn3@gmail.com>
2025-03-27 11:30:40 -0700

    Append elements to new container div and return it
    Created new container div and removed content div,
    as before it was appending to content div twice,
    first all the elements in content.js then in
    index.js. Also return container div so func
    doesn't return nothing

--- src/content.js
+++ src/content.js
@@ -1,28 +1,31 @@
 import SanjiImage from './sanji.png';
 
 export function homePage() {
-    const content = document.getElementById("content");
+    //const content = document.getElementById("content");
+    const container = document.createElement("div");
 
     const head = document.createElement("h1");
     head.classList.add("header");
     head.textContent = "Hello, this is the best restaurant";
-    content.appendChild(head);
+    container.appendChild(head);
 
     const paraText = document.createElement("p");
     paraText.classList.add("text");
     paraText.textContent = "Come eat some of the most fantastic food in the whole world at...";
-    content.appendChild(paraText);
+    container.appendChild(paraText);
 
     const newPara = document.createElement("p");
     newPara.style.fontStyle = 'italic';
     newPara.classList.add("text");
     newPara.textContent = "FOOD & STUFF!!!"
-    content.appendChild(newPara);
+    container.appendChild(newPara);
 
     const image = document.createElement("img");
     image.classList.add("image");
     image.src = SanjiImage;
     image.width = 300;
     image.height = 200;
-    content.appendChild(image);
+    container.appendChild(image);
+
+    return container;
 }
