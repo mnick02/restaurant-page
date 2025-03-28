a2d8b19ac7ba03272e90c50aeec68b1e6b61fbbc
mnick02 <nokiamn3@gmail.com>
2025-03-28 15:02:07 -0700

    Change to contact page, add func

--- src/content3.js
+++ src/content3.js
@@ -1,4 +1,26 @@
-export const three = document.createElement("div");
-three.classList.add("threeText");
-three.textContent = "Page Three";
-console.log("THREEY");
+
+export function contactPage() {
+    const container = document.createElement("div");
+
+    const head = document.createElement("h1");
+    head.classList.add("menuHead");
+    head.textContent = "Contact";
+    container.appendChild(head);
+
+
+    const contactContainer = document.createElement("div");
+    contactContainer.classList.add("item");
+    container.appendChild(contactContainer);
+
+    const contactName = document.createElement("h3");
+    contactName.classList.add("itemHead");
+    contactName.textContent = "Bobby Awesome Guy";
+    contactContainer.appendChild(contactName);
+
+    const description = document.createElement("p");
+    description.classList.add("description");
+    description.textContent = "Chef";
+    contactContainer.appendChild(description);
+
+    return container;
+}
