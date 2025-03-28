6aaceeaa67ff979dcc3875dd00bf7049fe86c664
mnick02 <nokiamn3@gmail.com>
2025-03-28 15:34:42 -0700

    Deployment commit

---
+++ src_content3_js.main.js
@@ -0,0 +1,22 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["src_content3_js"],{
+
+/***/ "./src/content3.js":
+/*!*************************!*\
+  !*** ./src/content3.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   contactPage: () => (/* binding */ contactPage)\n/* harmony export */ });\n\nfunction contactPage() {\n    const container = document.createElement(\"div\");\n\n    const head = document.createElement(\"h1\");\n    head.classList.add(\"menuHead\");\n    head.textContent = \"Contact\";\n    container.appendChild(head);\n\n\n    const contactContainer = document.createElement(\"div\");\n    contactContainer.classList.add(\"item\");\n    container.appendChild(contactContainer);\n\n    const contactName = document.createElement(\"h3\");\n    contactName.classList.add(\"itemHead\");\n    contactName.textContent = \"Bobby Awesome Guy\";\n    contactContainer.appendChild(contactName);\n\n    const description = document.createElement(\"p\");\n    description.classList.add(\"description\");\n    description.textContent = \"Chef\";\n    contactContainer.appendChild(description);\n\n    const description2 = document.createElement(\"p\");\n    description2.classList.add(\"description\");\n    description2.textContent = \"555-605-4456\";\n    contactContainer.appendChild(description2);\n    \n    const description3 = document.createElement(\"p\");\n    description3.classList.add(\"description\");\n    description3.textContent = \"awesomeGuy50@bmail.com\";\n    contactContainer.appendChild(description3);\n\n\n    const con2Container = document.createElement(\"div\");\n    con2Container.classList.add(\"item\");\n    container.appendChild(con2Container);\n\n    const conName = document.createElement(\"h3\");\n    conName.classList.add(\"itemHead\");\n    conName.textContent = \"Notty Real\";\n    con2Container.appendChild(conName);\n\n    const description4 = document.createElement(\"p\");\n    description4.classList.add(\"description\");\n    description4.textContent = \"Owner\";\n    con2Container.appendChild(description4);\n\n    const description5 = document.createElement(\"p\");\n    description5.classList.add(\"description\");\n    description5.textContent = \"554-555-2247\";\n    con2Container.appendChild(description5);\n    \n    const description6 = document.createElement(\"p\");\n    description6.classList.add(\"description\");\n    description6.textContent = \"notyRealer340@bmail.com\";\n    con2Container.appendChild(description6);\n\n    return container;\n}\n\n//# sourceURL=webpack:///./src/content3.js?");
+
+/***/ })
+
+}]);
