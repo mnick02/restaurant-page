fa1b3b360c07f84a23192e5611a1926e760d5308
mnick02 <nokiamn3@gmail.com>
2025-03-28 15:09:13 -0700

    Add another contact and its elements

--- src/content3.js
+++ src/content3.js
@@ -22,5 +22,40 @@ export function contactPage() {
     description.textContent = "Chef";
     contactContainer.appendChild(description);
 
+    const description2 = document.createElement("p");
+    description2.classList.add("description");
+    description2.textContent = "555-605-4456";
+    contactContainer.appendChild(description2);
+    
+    const description3 = document.createElement("p");
+    description3.classList.add("description");
+    description3.textContent = "awesomeGuy50@bmail.com";
+    contactContainer.appendChild(description3);
+
+
+    const con2Container = document.createElement("div");
+    con2Container.classList.add("item");
+    container.appendChild(con2Container);
+
+    const conName = document.createElement("h3");
+    conName.classList.add("itemHead");
+    conName.textContent = "Notty Real";
+    con2Container.appendChild(conName);
+
+    const description4 = document.createElement("p");
+    description4.classList.add("description");
+    description4.textContent = "Owner";
+    con2Container.appendChild(description4);
+
+    const description5 = document.createElement("p");
+    description5.classList.add("description");
+    description5.textContent = "554-555-2247";
+    con2Container.appendChild(description5);
+    
+    const description6 = document.createElement("p");
+    description6.classList.add("description");
+    description6.textContent = "notyRealer340@bmail.com";
+    con2Container.appendChild(description6);
+
     return container;
 }
